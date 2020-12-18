f6ac043099dce215b8f32353c43bfbbc661f24e9
Konrad Eder <53946421+Eder-K@users.noreply.github.com>
2020-12-18 15:21:03 +0100

    Replace PRECICE_ASSERT with MSVC-compliant version
    Update assertion such that it compiles when compiling preCICE on Windows (VS 2019), see #200

--- src/utils/assertion.hpp
+++ src/utils/assertion.hpp
@@ -21,20 +21,23 @@
 #include "Parallel.hpp"
 #include "stacktrace.hpp"
 
+/// Inclusion of this library is required for keywords 'or'/'and' to be used when compiling with MSVC.
+#include <ciso646>
+
 /// Helper macro, used by assertion.
 #define PRECICE_PRINT_ARGUMENT(r, data, i, elem) \
-  std::cerr << "  Argument " << i << ": " << elem << '\n';
+  BOOST_PP_IF(BOOST_PP_IS_EMPTY(elem), void, std::cerr << "  Argument " << i << ": " << elem << '\n');
 
 /// Asserts that expr evaluates to true, prints all other arguments and calls assert(false).
-#define PRECICE_ASSERT(...)                                                                                      \
-  if (not(BOOST_PP_VARIADIC_ELEM(0, __VA_ARGS__))) {                                                             \
+#define PRECICE_ASSERT(check, ...)                                                                              \
+  if (!(check)) {                                                                                                \
     std::cerr << "ASSERTION FAILED\n"                                                                            \
               << "  Location:          " << BOOST_CURRENT_FUNCTION << '\n'                                       \
               << "  File:              " << __FILE__ << ":" << __LINE__ << '\n'                                  \
               << "  Rank:              " << precice::utils::Parallel::getProcessRank() << '\n'                   \
-              << "  Failed expression: " << BOOST_PP_STRINGIZE(BOOST_PP_VARIADIC_ELEM(0, __VA_ARGS__))           \
+              << "  Failed expression: " << BOOST_PP_STRINGIZE(check)                                            \
               << '\n';                                                                                           \
-    BOOST_PP_SEQ_FOR_EACH_I(PRECICE_PRINT_ARGUMENT, , BOOST_PP_SEQ_TAIL(BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))); \
+    BOOST_PP_SEQ_FOR_EACH_I(PRECICE_PRINT_ARGUMENT, , BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__));                    \
     std::cerr << getStacktrace() << '\n';                                                                        \
     std::cerr.flush();                                                                                           \
     std::cout.flush();                                                                                           \
